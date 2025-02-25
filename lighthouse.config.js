4d839ef4013ab096df228a441677fd0b3f51aeed
Paul Wheatcroft <72826129+PaulWheatcroft@users.noreply.github.com>
2025-02-25 08:38:24 +0000

    Refactor Lighthouse CI configuration and add initial setup for automated performance testing

---
+++ lighthouse.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['https://paulwheatcroft.github.io/lighthouse-ci-demo'],
+      },
+      upload: {
+        target: 'github',
+      },
+    },
+  };
